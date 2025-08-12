4417d37f3be1eb932b89dbc3333d8cc956e7fb73
Richard Savage <richard@richardsavagevoice.com>
2025-08-12 18:58:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.set(1,1,50);
+camera.position.set(1,1,25);
 
 //Add lights to the scene, so we can actually see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
@@ -76,7 +76,7 @@ function animate() {
   //Make the eye move
 {
     //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    object.rotation.z = -3 + mouseX / window.innerWidth * 3;
     object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
   }
   renderer.render(scene, camera);
@@ -98,6 +98,7 @@ document.onmousemove = (e) => {
 //Start the 3D rendering
 
 animate();
+
 
 
 
